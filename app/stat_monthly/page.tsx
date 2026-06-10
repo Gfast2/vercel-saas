54e3d0e50743526f20e8bdfa99e1330d02d60969
Su Gao <gfast2@gmail.com>
2026-06-10 16:16:16 +0200

    feat:start placeholder new page to show chart about monthly service quantity

---
+++ app/stat_monthly/page.tsx
@@ -0,0 +1,8 @@
+export default async function StatMonthlyPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold mb-6">Monthly Statistics</h1>
+      <p>Monthly statistics will be displayed here.</p>
+    </div>
+  );
+}   
